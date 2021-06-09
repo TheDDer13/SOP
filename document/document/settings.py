82d2b990d0acd5ddc18eb9eedde6b9432d6c4bfb
TheDDer13 <tranphuongha1234@gmail.com>
2021-06-09 10:23:37 +0700

    Demo

--- document/document/settings.py
+++ document/document/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-#import django_heroku
+import django_heroku
 import dj_database_url
 from pathlib import Path
 import os
@@ -154,4 +154,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
